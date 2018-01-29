b613199e44bebfb6dfd02486ed41f599a64672f7
Yuki Tsuchida <d@gahaku.tech>
2018-01-29 17:43:20 +0000

    commit

--- app.rb
+++ app.rb
@@ -9,30 +9,33 @@ get '/' do
 end
 
 get '/view' do
-    url = params[:url]
-    html = open(url,:proxy => "http://177.74.61.240:8080").read
-    parsed_html = Nokogiri::HTML.parse(html,nil,'utf-8')
-    anchor = parsed_html.xpath("/html/body/div[1]/main/div[1]/section[1]/div[1]/div/div/div[1]/img")
-    image = anchor.attr("data-src").value
-    fileName = File.basename(image)
-    dirName = "public/images/"
-    filePath = dirName + fileName
+#     url = params[:url]
+#     html = open(url,:proxy => "http://175.208.122.10:80").read
+#     parsed_html = Nokogiri::HTML.parse(html,nil,'utf-8')
+#     anchor = parsed_html.xpath("/html/body/div[1]/main/div[1]/section[1]/div[1]/div/div/div[1]/img")
+#     image = anchor.attr("data-src").value
+#     fileName = File.basename(image)
+#     dirName = "public/images/"
+#     filePath = dirName + fileName
 
-  # write image adata
-  open(filePath, 'wb') do |output|
-    open(image) do |data|
-      output.write(data.read)
-       original = Magick::Image.read(filePath).first
-       original.format = "PNG"
-        reimage = original.matte_replace(0, 0)
-        pngfile = "#{filePath}.png"
-        reimage.write(pngfile)
-        front = Magick::Image.read(pngfile).first
-        back = Magick::Image.read('back.jpg').first
-       back.format = "PNG"
-        result = back.composite(front, 2100, 1000, Magick::OverCompositeOp)
-        result.write("public/images/result.png")
-    end
-  end
+#   # write image adata
+#   open(filePath, 'wb') do |output|
+#     open(image) do |data|
+#       output.write(data.read)
+#       original = Magick::Image.read(filePath).first
+#       original.format = "PNG"
+#         reimage = original.matte_replace(0, 0).resize(1024, 1024)
+#         reimage.write("public/images/result.png")
+    #     front = Magick::Image.read(pngfile).first
+    #     back = Magick::Image.read('back.jpg').first
+    #   back.format = "PNG"
+    #     if pngfile == "public/images/m50754108265_1.jpg?1515672731.png"
+    #         result = back.composite(front, 4720, 850, Magick::OverCompositeOp)
+    #   else
+    #         result = back.composite(front, 2100, 1000, Magick::OverCompositeOp)
+    #     end
+    #     result.write("public/images/result.png")
+#     end
+#   end
   erb :view
 end
